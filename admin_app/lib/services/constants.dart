8375ae20cdea91c17b7f14fd9262614ec16d3d93
Sujith Ram P <68140648+sujith6ram@users.noreply.github.com>
2025-07-22 22:17:29 +0530

    Update constants.dart

--- admin_app/lib/services/constants.dart
+++ admin_app/lib/services/constants.dart
@@ -1,6 +1,6 @@
 import 'package:web3dart/web3dart.dart';
 
-late String adminAddress = "0xF5C155E7615538198630684435B2fA2d633f935A";
+late String adminAddress;
 late EthereumAddress donorAddress;
 late EthereumAddress patientAddress;
 late String adminPrivateKey;
